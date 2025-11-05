2773cc371c2dd1d35e3c90a71360a4bb9752b155
Sabrina Xi <sabrina2018.xi@gmail.com>
2025-11-04 20:40:43 -0500

    updated vignette

--- tests/testthat/test-loadAndCleanData.R
+++ tests/testthat/test-loadAndCleanData.R
@@ -1,3 +1,8 @@
-test_that("LoadAndCleanData", {
-  expect_equal(2, 2)
+library(transComp)
+
+# Test that loadAndCleanData returns the correct data format
+test_that("loadAndCleanData() returns the correct output", {
+  filepath <- system.file("extdata", "example_data.csv", package = "transComp")
+  testData <- loadAndCleanData(filepath)
+  testthat::expect_length(testData, 7)
 })
